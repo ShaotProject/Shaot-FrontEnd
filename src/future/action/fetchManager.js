1d57374cee6c71770af3d31ac369f432227e13b9
Aleksandr Fesenko <fesenkoas@gmail.com>
2024-03-16 19:07:00 +0200

    16.03

--- src/future/action/fetchManager.js
+++ src/future/action/fetchManager.js
@@ -1,4 +1,3 @@
-
 import {
   loading,
   getCompanyById,
@@ -13,7 +12,7 @@ const baseURL = "https://shaotcloud.fly.dev/shaot";
 //Get Company By Id
 export const getCompanyByIdFetch = (id) => (dispatch) => {
   dispatch(loading(false));
-  fetch(`${baseURL}/company/${id}`, { method: "GET" })
+  fetch(`${baseURL}/company/${id}`, { method: "GET", mode: "cors" })
     .then((res) => res.json())
     .then((data) => {
       if (data.errorCode) {
@@ -24,7 +23,7 @@ export const getCompanyByIdFetch = (id) => (dispatch) => {
     })
     .catch((error) => {
       console.log(error);
-    })
+    });
 };
 
 // Create New Company
@@ -32,6 +31,7 @@ export const postNewCompany = (newData) => (dispatch) => {
   dispatch(loading(false));
   fetch(`${baseURL}/company`, {
     method: "POST",
+    mode: "cors",
     headers: {
       "Content-Type": "application/json", // Указываем, что отправляем JSON
     },
@@ -45,7 +45,7 @@ export const postNewCompany = (newData) => (dispatch) => {
     })
     .catch((error) => {
       console.log(error);
-    })
+    });
 };
 
 // Add Worker In Company
@@ -53,6 +53,7 @@ export const putEmployeeCompany = (id, employee) => (dispatch) => {
   dispatch(loading(false));
   fetch(`${baseURL}/company/${id}/worker/${employee}`, {
     method: "PUT",
+    mode: "cors",
     headers: {
       "Content-Type": "application/json",
     },
@@ -61,29 +62,30 @@ export const putEmployeeCompany = (id, employee) => (dispatch) => {
     .then((data) => dispatch(getMessage(data.message)))
     .catch((error) => {
       console.log(error);
-    })
+    });
 };
 
 //Get Company Schedule for week
 export const getCompanySchedule = (id) => (dispatch) => {
   dispatch(loading(false));
-  fetch(`${baseURL}/company/${id}/schedule`, { method: "GET"})
+  fetch(`${baseURL}/company/${id}/schedule`, { method: "GET", mode: "cors" })
     .then((res) => res.json())
     .then((data) => dispatch(сompanySchedule(data)))
     .catch((error) => {
       console.log(error);
-    })
+    });
 };
 
 //Delete Worker From Company
 export const deleteEmployeeCompany = (id, employee) => (dispatch) => {
   fetch(`${baseURL}/company/${id}/worker/${employee}`, {
     method: "DELETE",
+    mode: "cors",
   })
-  .then((response) => response.data)
-  .catch((error) => {
-    console.log(error);
-  })
+    .then((response) => response.data)
+    .catch((error) => {
+      console.log(error);
+    });
 };
 
 // Put Configurate Schedule
@@ -91,37 +93,42 @@ export const putConfigurateSchedule = (id, newData) => (dispatch) => {
   dispatch(loading(false));
   fetch(`${baseURL}/company/${id}/schedule/configure`, {
     method: "PUT",
+    mode: "cors",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(newData),
-  }).then((response) => response.json())
-  .catch((error) => {
-    console.log(error);
   })
+    .then((response) => response.json())
+    .catch((error) => {
+      console.log(error);
+    });
   // .then(data => {dispatch(getMessage(data.message))})
 };
 
 //Get Week Configurate
 export const getWeekConfigurate = (id) => (dispatch) => {
   dispatch(loading(false));
-  fetch(`${baseURL}/company/${id}/configuration`, { method: "GET" })
+  fetch(`${baseURL}/company/${id}/configuration`, {
+    method: "GET",
+    mode: "cors",
+  })
     .then((res) => res.json())
     .then((data) => dispatch(weekConfigurate(data)))
     .catch((error) => {
       console.log(error);
       dispatch(loading(false));
-    })
+    });
 };
 //Get Week Names
 export const fetchWeekNames = (id) => (dispatch) => {
   dispatch(loading(false));
-  fetch(`${baseURL}/company/${id}/week/names`, { method: "GET" })
+  fetch(`${baseURL}/company/${id}/week/names`, { method: "GET", mode: "cors" })
     .then((res) => res.json())
     .then((data) => dispatch(weekNames(data)))
     .catch((error) => {
       console.log(error);
-    })
+    });
 };
 
 //Post Week Names
@@ -131,6 +138,7 @@ export const fetchPutWeekNames = (id, newData) => (dispatch) => {
   dispatch(loading(false));
   fetch(`${baseURL}/company/${id}/week/period`, {
     method: "POST",
+    mode: "cors",
     headers: {
       "Content-Type": "application/json",
     },
@@ -139,11 +147,11 @@ export const fetchPutWeekNames = (id, newData) => (dispatch) => {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
-       dispatch(сompanySchedule(data))
+      dispatch(сompanySchedule(data));
     })
     .catch((error) => {
       console.log(error);
-    })
+    });
 };
 
 //Post Week Names
@@ -152,6 +160,7 @@ export const fetchPostSaveSchedule = (id, newData) => (dispatch) => {
   dispatch(loading(false));
   fetch(`${baseURL}/company/${id}/schedule/update`, {
     method: "PUT",
+    mode: "cors",
     headers: {
       "Content-Type": "application/json",
     },
@@ -159,9 +168,9 @@ export const fetchPostSaveSchedule = (id, newData) => (dispatch) => {
   })
     .then((response) => response.json())
     .then((data) => {
-      dispatch(сompanySchedule(data))
+      dispatch(сompanySchedule(data));
     })
     .catch((error) => {
       console.log(error);
-    })
+    });
 };
